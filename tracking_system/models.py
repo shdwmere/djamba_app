cdf455d30f6a9a9eb5ffb893d860773252029650
delphiu-m <caioszrm1@gmail.com>
2023-09-25 17:43:33 -0300

    create model

--- tracking_system/models.py
+++ tracking_system/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Pedidos(models.Model):
+    id = models.AutoField(primary_key=True)
+    id_pedido = models.CharField(max_length=12)
+    codigo_rastreio = models.CharField(max_length=14)
+    
+    cpf_cliente = models.CharField(max_length=14)
+    nome_cliente = models.CharField(max_length=100)
+    email_cliente = models.EmailField(max_length=100)
+    data_registro = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.nome_cliente
--- tracking_system/views.py
+++ tracking_system/views.py
@@ -1,5 +1,5 @@
 # produtos_files/views.py
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 
 
 def home(request):
