8eca6b9fd44fd1f7b1d0984aebe7931028216b42
shdW-js <caioszrm1@gmail.com>
2023-09-28 00:47:32 -0300

    migrated to mysql djamba_db

--- tools/gerador.py
+++ tools/gerador.py
@@ -1,10 +1,14 @@
 import random
 
-onze_chars = random.randint(11111111111, 99999999999)
-doze_chars = random.randint(111111111111, 999999999999)
 
-id_pedido = '#' + str(onze_chars)
-codigo_rastreio = 'BR' + str(doze_chars)
 
-print('ID do pedido: \033[4;32m{}\033[0m'.format(id_pedido))
-print('Código de rastreio: \033[4;32m{}\033[0m'.format(codigo_rastreio))
+for _ in range(45):
+    onze_chars = random.randint(11111111111, 99999999999)
+    doze_chars = random.randint(111111111111, 999999999999)
+
+    id_pedido = '#' + str(onze_chars)
+    codigo_rastreio = 'BR' + str(doze_chars)
+
+    # Imprima os valores para cada repetição
+    print('Código de rastreio: \033[4;32m{}\033[0m'.format(codigo_rastreio))
+    #print('ID do pedido: \033[4;32m{}\033[0m'.format(id_pedido))
